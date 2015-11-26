c8be44575c51c705f0462e0b15509b95893258d2
candale <candale.andrei@gmail.com>
2015-11-26 14:17:31 +0000

    Removed old structure

--- items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class CarItem(scrapy.Item):
-    title = scrapy.Field()
-    url = scrapy.Field()
-    price = scrapy.Field()
-    seller_info = scrapy.Field()
-    photos_urls = scrapy.Field()
