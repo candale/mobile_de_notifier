9c40ba5f050164c2fede689272cc25d09b2bce16
candale <candale.andrei@gmail.com>
2016-02-13 18:06:24 +0000

    Added gunicorn run file

---
+++ run/serve_mobile_de_notifier.sh
@@ -0,0 +1,31 @@
+
+NAME="mobile_de_notifier"  
+DJANGODIR=/root/workspace/virtualenvs/car_scraping/mobile_de_notifier  
+SOCKFILE=/root/workspace/virtualenvs/car_scraping/run/gunicorn.sock  
+USER=root                                       
+# GROUP=webapps                                
+NUM_WORKERS=9                                 
+DJANGO_SETTINGS_MODULE=mobile_de_notifier.settings 
+DJANGO_WSGI_MODULE=mobile_de_notifier.wsgi 
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
